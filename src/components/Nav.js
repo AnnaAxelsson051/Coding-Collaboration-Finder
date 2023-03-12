c71f64ccf6df9a66bd194cb17e08cecdaced78b2
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-12 23:49:23 +0100

    Add input form labels for signing up

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 /*What only logged in users see*/
 import whiteLogo from '../images/tinder-logo-white.png'
 import colorLogo from '../images/tinder-logo-color.png'
-const Nav = ({minimal, authToken, setShowModal, showModal, setIsSignUp}) => {
+const Nav = ({minimal, setShowModal, showModal, setIsSignUp}) => {
     /* pass in minimal and if minimal is false ge get the standard logo (red)
     * otherwise if its not minimal we return the white logo
     * we also want a second button to sign up, that only shows up if we are not logged in*/
@@ -9,6 +9,8 @@ const Nav = ({minimal, authToken, setShowModal, showModal, setIsSignUp}) => {
         setShowModal(true)
         setIsSignUp(false) /*if we are logging in*/
     }
+
+    const authToken = false
 
     return (
         <nav>
--- src/pages/OnBoarding.js
+++ src/pages/OnBoarding.js
@@ -1,8 +1,150 @@
-
+import {useState} from 'react'
+import Nav from '../components/Nav'
 
 const OnBoarding = () => {
+
+    const handleSubmit = () =>{
+        console.log('submitted')
+    }
+    const handleChange = () =>{
+        console.log('change')
+    }
     return (
-        <div>OnBoarding</div>
+        <>
+            <Nav
+            minimal={true}
+            setShowModal={() => {}}
+                showModal={false}
+            />
+            <div className="onboarding">
+             <h2>Create Account</h2>
+                <form onSubmit={handleSubmit}>
+                    <section>
+                        <label htmlFor="first_name">First Name</label>
+                        <input
+                        id="first-name"
+                        type="text"
+                        name="first_name"
+                        placeholder="First Name"
+                        required={true}
+                        value={""}
+                        onChange={handleChange}
+
+                        />
+
+                        <label>Birthday</label>
+                        <div className="multiple-input-container">
+                        <input
+                            id="dob_day"
+                            type="number"
+                            name="dob_day"
+                            placeholder="DD"
+                            required={true}
+                            value={""}
+                            onChange={handleChange}
+                        />
+
+                        <input
+                            id="dob_month"
+                            type="number"
+                            name="dob_month"
+                            placeholder="MM"
+                            required={true}
+                            value={""}
+                            onChange={handleChange}
+                        />
+
+                        <input
+                            id="dob_year"
+                            type="number"
+                            name="dob_year"
+                            placeholder="YYYY"
+                            required={true}
+                            value={""}
+                            onChange={handleChange}
+                        />
+                        </div>
+
+                        <label>Gender</label>
+                        <div className="multiple-input-container">
+                            <input
+                                id="man-gender-identity"
+                                type="radio"
+                                name="gender_identity"
+                                value="man"
+                                onChange={handleChange}
+                                checked={false}
+                            />
+                            <label htmlFor="man-gender-identity">Man</label>
+                            <input
+                                id="woman-gender-identity"
+                                type="radio"
+                                name="gender_identity"
+                                value="woman"
+                                onChange={handleChange}
+                                checked={false}
+                            />
+                            <label htmlFor="woman-gender-identity">Woman</label>
+                            <input
+                                id="more-gender-identity"
+                                type="radio"
+                                name="gender_identity"
+                                value="more"
+                                onChange={handleChange}
+                                checked={false}
+                            />
+                            <label htmlFor="more-gender-identity">More</label>
+                            </div>
+
+                        <label htmlFor="show-gender">Show my gender on profile</label>
+                        <input
+                            id="show-gender"
+                            type="checkbox"
+                            name="show_gender"
+                            onChange={handleChange}
+                            checked={false}
+                        />
+
+
+
+                        <label>Show me</label>
+                        <div className="multiple-input-container">
+                        <input
+                            id="man-gender-interest"
+                            type="radio"
+                            name="gender_interest"
+                            value="man"
+                            onChange={handleChange}
+                            checked={false}
+                        />
+                        <label htmlFor="man-gender-interest">Man</label>
+                        <input
+                            id="woman-gender-interest"
+                            type="radio"
+                            name="gender_interest"
+                            value="woman"
+                            onChange={handleChange}
+                            checked={false}
+                        />
+                        <label htmlFor="woman-gender-interest">Woman</label>
+                        <input
+                            id="more-gender-interest"
+                            type="radio"
+                            name="gender_interest"
+                            value="everyone"
+                            onChange={handleChange}
+                            checked={false}
+                        />
+                        <label htmlFor="more-gender-identity">Everyone</label>
+                        </div>
+
+                        <label htmlFor="about">About Me</label>
+                    </section>
+
+                </form>
+            </div>
+        </>
+
     )
 }
 export default OnBoarding
