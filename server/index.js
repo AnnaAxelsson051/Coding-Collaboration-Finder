7d9c8ff38569a1e5bd080e19019f57376f865575
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-17 20:40:15 +0100

    Add log in functionality

--- server/index.js
+++ server/index.js
@@ -54,9 +54,29 @@ app.post('/signup', async (req,res) =>{
     }
 })
 
-app.post('/login', req, res) =>{
+app.post('/login', async( req, res) =>{
     const client = new MongoClient(uri)
-    const{email,password} = req.body
+    const {email,password} = req.body
+
+    try{
+    await client.connect()
+    const database = client.db('app-data')
+    const users = database.collection('users')
+
+    const user = await users.findOne({email})
+
+        const correctPassword = await bcrypt.compare(password, user.hashed_password)
+
+    if(user && correctPassword){
+    const token = jwt.sign(user,email, {
+        expiresIn: 60 * 24
+    })
+            res.status(201).json({token, userId: user.user_id, email})
+    }
+    res.status(400).send('Invalid Credentials')
+    }catch(err){
+        console.log(err)
+    }
 
 } )
 
