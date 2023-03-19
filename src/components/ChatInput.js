f1a78231c1e0ddb4327a7c477e75fc0f390be719
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-19 18:06:35 +0100

    Create timestamp for messages

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,7 +1,21 @@
 import {useState} from 'react'
 
-const ChatInput = () =>{
+const ChatInput = ({user, clickedUser, getUsersMessages, getClickedUsersMessages}) =>{
     const[textArea, setTextArea] = useState(null)
+    const userId = user?.user_id
+    const clickedUserId = clickedUser?.user_id
+
+    //creating timestamp
+    const addMessage = async () => {
+        const message = {
+            timestamp: new Date().toISOString(),
+            from_userId: userId,
+            to_userId: clickedUserId,
+            message: textArea
+
+        }
+    }
+
     return (
         <div className="chat-input">
             <textarea value={textArea} onChange={(e) => setTextArea(e.target.value)}/>
