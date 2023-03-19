48ade0dd93f9f934838c98d4e475bad48126d64f
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-19 17:21:10 +0100

    Add functionality for receiving and sending messages

--- src/components/ChatDisplay.js
+++ src/components/ChatDisplay.js
@@ -7,12 +7,15 @@ const ChatDisplay = ({user, clickedUser}) => {
     const userId = user?.user_id
     const clickedUserId = clickedUser?.user_id
     const [usersMessages, setUsersMessages] = useState(null)
+    const [clickedUsersMessages, setClickedUsersMessages] = useState(null)
 
-    const getUsersMessages = async () => {
+    const getMessages = async (senderId, recipientId) => {
         try{
         const response = await axios.get('http://localhost:8000/messages',{
-            params: {userId: userId, correspondingUserId: clickedUserId }
+            params: {userId: senderId, correspondingUserId: recipientId }
         })
+            return response.data
+
     /*look for anything to and from the one clicked on*/
     setUsersMessages(response.data)
     } catch(error) {
@@ -21,10 +24,10 @@ const ChatDisplay = ({user, clickedUser}) => {
 }
 
 useEffect(() => {
-    getUsersMessages()
-},[usersMessages])
+    setUsersMessages(getMessages(userId, clickedUserId))
+    setClickedUsersMessages(getMessages(clickedUserId, userId))
+},[usersMessages, clickedUsersMessages])
 
-console.log(usersMessages)
 
     return (
         <>
