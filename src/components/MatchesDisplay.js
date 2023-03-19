f68014642fe1925738105b603fa7458ac3ae9e08
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-19 19:56:13 +0100

    Update filter users functionality

--- src/components/MatchesDisplay.js
+++ src/components/MatchesDisplay.js
@@ -1,12 +1,15 @@
 import axios from 'axios'
 import {useEffect, useState} from 'react'
+import {useCookies} from 'react-cookie'
 
 const MatchesDisplay = ({matches, setClickedUser}) =>{
     const [matchedProfiles, setMatchedProfiles] = useState(null)
+    const [cookies, setCookie, removeCookie] = useCookies(null)
 
     /*pass tru array of user ids */
 
     const matchedUserIds = matches.map(({ user_id}) => user_id)
+    const userId = cookies.UserId
     const getMatches = async () =>{
         try {
             const response = await axios.get('http://localhost:8000/users', {
@@ -22,12 +25,18 @@ const MatchesDisplay = ({matches, setClickedUser}) =>{
         getMatches ()
     },[matches])
 
+    //filter so communicatio is only possible with matches
+const filteredMatchedProfiles = matchedProfiles?.filter(
+    (matchedProfile) =>
+        matchedProfile.matches.filter((profile) => profile.user_id == userId).length > 0)
 
     return (
         <div className="matches-display">
-            {matchedProfiles?.map((match) => (
+            {filteredMatchedProfiles?.map((match, _index) => (
                 <div
-                    key={match.user_id} className="match-card" onClick={() => setClickedUser(match)}>
+                    key={_index}
+                    className="match-card"
+                    onClick={() => setClickedUser(match)}>
                     <div className="img-container">
                         <img src={match?.url} alt={match?.first_name + " profile"} />
                     </div>
